38776505aef5205a52349e7913087a84a217e482
吴嘉烽 <wjf1@meitu.com>
2018-12-18 14:41:43 +0800

    feat:Threeway quick sort

--- sort/quicksort/quicksort.go
+++ sort/quicksort/quicksort.go
@@ -2,39 +2,37 @@ package quicksort
 
 import "fmt"
 
-func QuicksortDESC(list []int) []int  {
+func QuicksortDESC(list []int) []int {
     lenList := len(list)
     if lenList <= 1 {
-        return  list
+        return list
     }
     
     standard := list[0]
     start := 1
     end := lenList - 1
     for start < end {
-        for list[end] < standard {
+        for list[end] < standard && start < end {
             end --
         }
         
-        for list[start] > standard {
+        for list[start] > standard && start < end {
             start ++
         }
         
-        if end < start {
-            t := list[start]
-            list[start] = list[end]
-            list[end] = t
+        if list[end] > list[start] {
+            swap(list, start, end)
             start ++
             end --
         }
     }
     
-    if list[start] > standard{
+    if list[start] > standard {
         list[0] = list[start]
         list[start] = standard
-    }else{
-        list[0] = list[start - 1]
-        list[start - 1] = standard
+    } else {
+        list[0] = list[start-1]
+        list[start-1] = standard
     }
     
     list = append(QuicksortDESC(list[:start]), QuicksortDESC(list[start:])...)
@@ -43,7 +41,6 @@ func QuicksortDESC(list []int) []int  {
 
 func QuicksortASC(list []int) []int {
     fmt.Println("")
-    println("")
     maxList := len(list)
     if maxList <= 1 {
         //一个数
@@ -54,7 +51,7 @@ func QuicksortASC(list []int) []int {
     end := maxList - 1
     standard := list[0]
     for start < end {
-        for list[end] > standard && start < end{
+        for list[end] > standard && start < end {
             end --
         }
         
@@ -62,23 +59,94 @@ func QuicksortASC(list []int) []int {
             start ++
         }
         if list[end] < list[start] {
-            t := list[start]
-            list[start] = list[end]
-            list[end] = t
+            swap(list, start, end)
             start ++
             end --
         }
     }
-    if list[start] < standard{
+    if list[start] < standard {
         list[0] = list[start]
         list[start] = standard
-    }else{
-        list[0] = list[start - 1]
-        list[start - 1] = standard
+    } else {
+        list[0] = list[start-1]
+        list[start-1] = standard
     }
-
-    list = append(QuicksortASC(list[:start]), QuicksortASC(list[start :])...)
+    
+    list = append(QuicksortASC(list[:start]), QuicksortASC(list[start:])...)
     
     return list
 }
 
+func ThreeWayQuickSort(a []int, left, right int) []int {
+    threeWayQuickSort(a)
+    return a
+}
+
+//三路快速排序
+func threeWayQuickSort(a []int) {
+    if len(a) <= 1 {
+        return
+    }
+    
+    i, p, left := 0, 0, 0
+    right := len(a) - 1
+    j, q := right-1, right-1
+    pivot := right
+    
+    for {
+        for i <= j && a[i] <= a[pivot] {
+            if a[i] == a[pivot] {
+                swap(a, i, p)
+                p++
+            }
+            i ++
+        }
+        
+        for j >= i && a[j] >= a[pivot] {
+            if a[j] == a[pivot] {
+                swap(a, j, q)
+                q --
+            }
+            j--
+        }
+        if i >= j {
+            break
+        }
+        swap(a, i, j)
+        i++
+        j--
+    }
+    // 此时得到的顺序
+    // |等于|小于|大于|等于|
+    // left p  i j  q  right
+    //将等于的值交换到中间
+    //处理的时候用的后面一个值，此处需要退1
+    i--
+    p--
+    for p >= left {
+        swap(a, p, i)
+        p--
+        i--
+    }
+    j++
+    q++
+    for q <= right {
+        swap(a, q, j)
+        q++
+        j++
+    }
+    
+    //得到
+    //|小于|等于|大于|
+    //递归排序 小于和大于的值
+    threeWayQuickSort(a[:i+1])
+    threeWayQuickSort(a[j-1:])
+}
+
+func swap(a []int, i, j int) {
+    if i != j {
+        temp := a[i]
+        a[i] = a[j]
+        a[j] = temp
+    }
+}
--- sort/quicksort/quicksort_test.go
+++ sort/quicksort/quicksort_test.go
@@ -28,25 +28,55 @@ func TestQuicksortASC(t *testing.T) {
     }
 }
 
-//func TestQuicksortDESC(t *testing.T) {
-//    type args struct {
-//        list []int
-//    }
-//    tests := []struct {
-//        name string
-//        args args
-//        want []int
-//    }{
-//        // TODO: Add test cases.
-//        {args:args{[]int{123, 1, 213, 21, 42, 1321}}, want: []int{1321,213,123,42,21,1}},
-//        {args:args{[]int{1,32,4,1,23,42,141,2}}, want: []int{141,24,32,23,4,2,1}},
-//        {args:args{[]int{0,2,0}}, want: []int{2,0,0}},
-//    }
-//    for _, tt := range tests {
-//        t.Run(tt.name, func(t *testing.T) {
-//            if got := QuicksortDESC(tt.args.list); !reflect.DeepEqual(got, tt.want) {
-//                t.Errorf("QuicksortASC() = %v, want %v", got, tt.want)
-//            }
-//        })
-//    }
-//}
+func TestQuicksortDESC(t *testing.T) {
+    type args struct {
+        list []int
+    }
+    tests := []struct {
+        name string
+        args args
+        want []int
+    }{
+        // TODO: Add test cases.
+        {args:args{[]int{123, 1, 213, 21, 42, 1321}}, want: []int{1321,213,123,42,21,1}},
+        {args:args{[]int{1,32,4,1,23,42,141,2}}, want: []int{141,42, 32,23,4,2,1,1}},
+        {args:args{[]int{0,2,0}}, want: []int{2,0,0}},
+        {args:args{[]int{0,0,2}}, want: []int{2,0,0}},
+        {args:args{[]int{0,2}}, want: []int{2,0}},
+        {args:args{[]int{2,0}}, want: []int{2,0}},
+    }
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            if got := QuicksortDESC(tt.args.list); !reflect.DeepEqual(got, tt.want) {
+                t.Errorf("QuicksortASC() = %v, want %v", got, tt.want)
+            }
+        })
+    }
+}
+
+func TestThreeWayQuickSort(t *testing.T) {
+    
+    type args struct {
+        list []int
+    }
+    tests := []struct {
+        name string
+        args args
+        want []int
+    }{
+        // TODO: Add test cases.
+        {args:args{[]int{4, 1, 4, 4, 3, 5,8,3,3,4}}, want: []int{1,3,3,3,4,4,4,4,5,8}},
+        {args:args{[]int{1,32,4,1,23,42,141,2}}, want: []int{1,1,2,4,23,32,42,141}},
+        {args:args{[]int{0,2,0}}, want: []int{0,0,2}},
+        {args:args{[]int{0,0,2}}, want: []int{0,0,2}},
+        {args:args{[]int{2,0,0}}, want: []int{0,0,2}},
+        {args:args{[]int{2,2,0}}, want: []int{0,2,2}},
+    }
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            if got := ThreeWayQuickSort(tt.args.list, 0, len(tt.args.list) - 1); !reflect.DeepEqual(got, tt.want) {
+                t.Errorf("ThreeWayQuickSort() = %v, want %v", got, tt.want)
+            }
+        })
+    }
+}
